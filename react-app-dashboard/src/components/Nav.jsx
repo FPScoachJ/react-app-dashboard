1a74ff4eeba1b493a23d6c6187212ede76b808d6
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-06-27 08:29:19 -0700

    Initial Commit

---
+++ react-app-dashboard/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+  return (
+    <nav className="flex flex-col border-solid border-black border-[5px] w-80 h-screen">
+      <p>Nav1</p>
+      <p>Nav2</p>
+      <p>Nav3</p>
+    </nav>
+  );
+}
