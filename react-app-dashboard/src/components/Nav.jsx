be101f112acc952bd339f516ca6e531f63b3670f
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-06-27 12:55:55 -0700

    Afternoon commit

--- react-app-dashboard/src/components/Nav.jsx
+++ react-app-dashboard/src/components/Nav.jsx
@@ -1,19 +1,32 @@
+/* eslint-disable react/prop-types */
 import NavLink from "./NavLink";
+import dashboard from "../assets/Icons/dashboard.png";
+import customers from "../assets/Icons/customers.png";
+import message from "../assets/Icons/message.png";
+import help from "../assets/Icons/help.png";
+import settings from "../assets/Icons/settings.png";
+import password from "../assets/Icons/password.png";
+import signout from "../assets/Icons/signout.png";
+import logo from "../assets/Icons/logo.png";
+import hamburger from "../assets/Icons/hamburger.png";
 
 const Nav = () => {
+    
   const links = [
-    { name: "Dashboard" },
-    { name: "Customers" },
-    { name: "Message" },
-    { name: "Help" },
-    { name: "Settings" },
-    { name: "Password" },
-    { name: "Sign Out" },
+    { name: "Dashboard", icon: dashboard },
+    { name: "Customers", icon: customers },
+    { name: "Message", icon: message },
+    { name: "Help", icon: help },
+    { name: "Settings", icon: settings },
+    { name: "Password", icon: password },
+    { name: "Sign Out", icon: signout },
   ];
   return (
     <nav className="flex flex-col w-80 h-screen bg-regal-blue pl-3 gap-10">
-      <div>
-        <h1 className="text-white text-2xl">Brand Name</h1>
+      <div className="flex justify-around">
+        <img className="h-10 w-10 mr-2" src={logo} alt="" />
+        <h1 className="text-white text-2xl pr-10">Brand Name</h1>
+        <img className="h-10 w-10 mr-2" src={hamburger} alt="" />
       </div>
       <div className="flex flex-col gap5 ">
         {links.map((link) => (
--- react-app-dashboard/src/components/NavLink.jsx
+++ react-app-dashboard/src/components/NavLink.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 const NavLink = ({ link }) => {
   return (
-    <div className='text-white hover:bg-white hover:text-regal-blue ease-in duration-200 h-10 flex items-center pl-2 rounded-l-full hover:cursor-pointer'>
+    <div className="text-white hover:bg-white hover:text-regal-blue ease-in duration-200 h-10 flex items-center pl-2 rounded-l-full hover:cursor-pointer">
+      <img className="h-5 w-5 mr-2" src={link.icon} alt="" />
       {link.name}
     </div>
   );
